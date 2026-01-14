064f5f697663043a6f3857599d04800a9d643373
anisurarzu <shakibanis1234@gmail.com>
2026-01-14 19:33:24 +0100

    fix the schoalrship issue

--- routes/scholarshipRoutes.js
+++ routes/scholarshipRoutes.js
@@ -11,11 +11,11 @@ const {
   deleteScholarship,
 } = require("../controllers/scholarshipController");
 
-router.post("/scholarship-info", verifyAuthToken, createScholarship);
-router.get("/scholarship-info", verifyAuthToken, getAllScholarships);
+router.post("/scholarship-info", createScholarship);// Public endpoint
+router.get("/scholarship-info", getAllScholarships); // Public endpoint
 router.get("/scholarship-info-old", verifyAuthToken, getOldScholarships);
 router.get("/scholarship-info-old-2025", verifyAuthToken, getOld2025Scholarships);
-router.get("/scholarship-info/:id", getScholarshipById);
+router.get("/scholarship-info/:id", getScholarshipById); // Already public
 router.put("/scholarship-info/:id", updateScholarship);
 router.delete("/scholarship-info/:id", deleteScholarship);
 
